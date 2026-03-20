e46d8527e7447e962c8dd824e13c6f997528f18f
duzz17 <ducn69279@gmail.com>
2026-03-20 16:05:23 +0700

    update frontend

--- frontend/src/pages/DocumentsPage.js
+++ frontend/src/pages/DocumentsPage.js
@@ -56,14 +56,24 @@ function DocumentsPage() {
 
     files = [
       {
-        name: "Chế độ tài sản vợ chồng.pdf",
-        url: process.env.PUBLIC_URL + "/files/thesis1.pdf",
+        name: "2023 - Chế độ tài sản của vợ chồng trong pháp luật Việt Nam - Do Hoang Thu Huong 1905LHOC028.pdf",
+        url: process.env.PUBLIC_URL + "/files/luanvan1.pdf",
         type: "pdf",
       },
       {
-        name: "Đào tạo cán bộ công chức.docx",
-        url: process.env.PUBLIC_URL + "/files/thesis2.docx",
-        type: "word",
+        name: "2023 - DTNC. 509 - Công tác đào tạo, bồi dưỡng cán bộ, công chức cấp xã tại huyện Yên Định, tỉnh Thanh Hóa - NguyenNgocSon.pdf",
+        url: process.env.PUBLIC_URL + "/files/luanvan2.pdf",
+        type: "pdf",
+      },
+      {
+        name: "2023 - Pháp luật về xác định quyền sử dụng đất của thành viên hộ gia đình ở Việt Nam hiện nay - KL. 1009 Dinh Van Cong 1905LHOB009.pdf",
+        url: process.env.PUBLIC_URL + "/files/luanvan3.pdf",
+        type: "pdf",
+      },
+      {
+        name: "2023 - Quy định về thừa kế theo pháp luật trong Bộ luật dân sự Việt Nam hiện hành: Khóa luận tốt nghiệp ngành Luật học Khoa Nhà nước và Pháp luật -KL. 1004 Dang Thi Bich Phuong 1905LHOB052.pdf",
+        url: process.env.PUBLIC_URL + "/files/luanvan4.pdf",
+        type: "pdf",
       },
     ];
   }
@@ -75,12 +85,12 @@ function DocumentsPage() {
     files = [
       {
         name: "Kỷ yếu hội thảo 2024.pdf",
-        url: process.env.PUBLIC_URL + "/files/book1.pdf",
+        url: process.env.PUBLIC_URL + "/files/giaotrinh1.pdf",
         type: "pdf",
       },
       {
         name: "Cấu trúc bài NCKH.pdf",
-        url: process.env.PUBLIC_URL + "/files/book2.pdf",
+        url: process.env.PUBLIC_URL + "/files/giaotrinh2.pdf",
         type: "pdf",
       },
     ];
